68e008a855ef2c7e5e093f8cb267e2bffe129039
Leonardo Villalobos <artist_artistian@hotmail.com>
2015-10-19 02:48:43 -0600

    Indexador completamente serializable
    Puede ser cargado y guardado en memoria con todos sus componentes. Aún
    no se ha implementado manejo de bloques en postingsList.

--- workspace/WebcrawlerTest/src/test/Indexer.java
+++ workspace/WebcrawlerTest/src/test/Indexer.java
@@ -2,13 +2,20 @@ package test;
 
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.Vector;
 
 import test.PostingsList.PostingsListElement;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class Indexer
+public class Indexer implements Serializable
 {
 	public HashMap<String, Integer> docList;
 	public PostingsList pList;
@@ -22,6 +29,72 @@ public class Indexer
 		wDict = new Dictionary();
 		nextID = 0;
 	}
+	/*
+	public void load()
+	{
+		File docListFile = new File("docs.ser");
+		if(docListFile.exists())
+		{
+			try
+			{
+				FileInputStream file = new FileInputStream("docs.ser");
+				ObjectInputStream input = new ObjectInputStream(file);
+				docList = (HashMap<String, Integer>) input.readObject();
+				input.close();
+			}catch (IOException | ClassNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			docList = new HashMap<String, Integer>();
+		}
+		
+		File dictionaryFile = new File("dict.ser");
+		if(dictionaryFile.exists())
+		{
+			try
+			{
+				FileInputStream file = new FileInputStream("dict.ser");
+				ObjectInputStream input = new ObjectInputStream(file);
+				wDict = (Dictionary) input.readObject();
+				input.close();
+			}catch (IOException | ClassNotFoundException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			wDict = new Dictionary();
+		}
+	}
+	
+	public void save()
+	{
+		try
+		{  
+			FileOutputStream file = new FileOutputStream("docs.ser");
+			ObjectOutputStream output = new ObjectOutputStream(file);   
+			output.writeObject(docList);
+			output.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		
+		try
+		{  
+			FileOutputStream file = new FileOutputStream("dict.ser");
+			ObjectOutputStream output = new ObjectOutputStream(file);   
+			output.writeObject(wDict);
+			output.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}*/
 	
 	public void parseDocument(String path) throws FileNotFoundException
 	{
@@ -61,5 +134,15 @@ public class Indexer
 			}
 			sc.close();
 		}
+		else
+			System.out.println(path);
+	}
+	
+	public int fileID(String path)
+	{
+		int res = 0;
+		if(docList.containsKey(path))
+			res = docList.get(path);
+		return res;
 	}
 }
--- workspace/WebcrawlerTest/src/test/PostingsList.java
+++ workspace/WebcrawlerTest/src/test/PostingsList.java
@@ -1,10 +1,18 @@
 package test;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Vector;
 
-public class PostingsList
+public class PostingsList implements Serializable
 {
-	public class PostingsListElement
+	public class PostingsListElement implements Serializable
 	{
 		public int docID;
 		public int termID;
@@ -16,7 +24,7 @@ public class PostingsList
 		}
 	}
 	
-	public Vector<PostingsListElement> postings;
+	public transient Vector<PostingsListElement> postings;
 	
 	public PostingsList()
 	{
@@ -36,5 +44,33 @@ public class PostingsList
 	public String toString()
 	{
 		return postings.toString();
+	}
+	
+	public void save()
+	{
+		try
+		{  
+			FileOutputStream file = new FileOutputStream("0.ser");
+			ObjectOutputStream output = new ObjectOutputStream(file);   
+			output.writeObject(postings);
+			output.close();
+		}catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	public void load()
+	{
+		try
+		{
+			FileInputStream file = new FileInputStream("0.ser");
+			ObjectInputStream input = new ObjectInputStream(file);
+			postings = (Vector<PostingsListElement>) input.readObject();
+			input.close();
+		}catch (IOException | ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
