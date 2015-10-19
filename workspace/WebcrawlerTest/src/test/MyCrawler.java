6d67f6e1196d4710dbd5c9c718bc371380ae3abd
Juan Carlos Porras <juan-ca-pq@hotmail.com>
2015-10-18 23:55:29 -0600

    Ya no hace archivos vacios. Correcto manejo de los titulos de archivos generados.

--- workspace/WebcrawlerTest/src/test/MyCrawler.java
+++ workspace/WebcrawlerTest/src/test/MyCrawler.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -49,7 +51,10 @@ public class MyCrawler extends WebCrawler {
             	Document doc = Jsoup.parse(line);
             	Elements paragraphs = doc.select("p");
                 String [] urlTokens = url.split("/");
-                File file = new File(directory, urlTokens[urlTokens.length-1] + ".txt");
+                String title = urlTokens[urlTokens.length-1] + ".txt";
+                title = title.replaceAll("[\\/\\\\:\\*\\?\\\"<>\\|]","-");
+                File file = new File(directory, title);
+                //File file = new File(directory, documentsCreated + ".txt");
                 FileWriter fw = new FileWriter(file);
             	PrintWriter pw = new PrintWriter(fw);
             	  for(Element p : paragraphs){
